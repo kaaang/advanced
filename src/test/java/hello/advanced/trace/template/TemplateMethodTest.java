8e8b5440efb99ef5291743678153667e3c80fcc0
kaaang <kaaang@kakao.com>
2022-09-29 23:15:52 +0900

    templateMethod external

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -49,4 +49,23 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1실행");
+            }
+        };
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2실행");
+            }
+        };
+        template2.execute();
+    }
+
 }
